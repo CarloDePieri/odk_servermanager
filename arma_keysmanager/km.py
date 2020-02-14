899d382ccc3d494c00a0bf84d5ba2ba6db13b97c
Carlo De Pieri <depieri.carlo@gmail.com>
2020-02-14 12:54:45 +0100

    refactored the check for a key file

--- arma_keysmanager/km.py
+++ arma_keysmanager/km.py
@@ -16,10 +16,14 @@ def parse_preset(filename: str) -> List[str]:
         mods_data ))
     return mods_name
 
+def is_keyfile(filename: str) -> bool:
+    """Check if a file seems to be an Arma3 mod key."""
+    file_ext = os.path.splitext(filename)[-1].lower()
+    return os.path.isfile(filename) and (file_ext == ".bikey")
+
 def clear_keys_folder(folder_name: str) -> None:
     """Delete every key in the Keys folder."""
     for filename in os.listdir(folder_name):
         file_path = os.path.join(folder_name, filename)
-        file_ext = os.path.splitext(file_path)[-1].lower()
-        if os.path.isfile(file_path) and (file_ext == ".bikey"):
+        if is_keyfile(file_path):
             os.unlink(file_path)
