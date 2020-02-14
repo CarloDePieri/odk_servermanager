e4e6e7a8c732d6e09a2b014e8b4efaad57f4a907
Carlo De Pieri <depieri.carlo@gmail.com>
2020-02-14 11:53:23 +0100

    expanded the parse mod preset output testing

--- tests/test_km.py
+++ tests/test_km.py
@@ -5,10 +5,14 @@ preset_file_name = "tests/preset.html"
 
 class TestPresetImporting:
 
-    def test_should_parse_all_mods(self):
+    def test_should_parse_all_mods(self, reset_folder_structure):
+        """The preset mechanism should parse all mod in the preset"""
         mods = parse_preset(preset_file_name)
         assert len(mods) == 4
 
     def test_should_return_a_list_of_mod_names(self):
+        """The preset mechanism should return a list of mod names"""
         mods_name = parse_preset(preset_file_name)
+        assert isinstance(mods_name, list)
+        assert isinstance(mods_name[0], str)
         assert "ODKAI" in mods_name
