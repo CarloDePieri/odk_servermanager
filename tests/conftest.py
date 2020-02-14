04cb3eb4946aa9a254b4e8dfc80b56e9f51d94e5
Carlo De Pieri <depieri.carlo@gmail.com>
2020-02-14 11:54:29 +0100

    test framework can now create and reset a folder structure to test with

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+import shutil
+import zipfile
+
+
+test_folder_structure_name = "tests/Arma"
+test_folder_structure_zip = "tests/folder_structure.zip"
+
+def _reset_folder_structure():
+    """Reset the folder structure to test on"""
+    # Delete the old folder if present
+    if os.path.isdir(test_folder_structure_name):
+        shutil.rmtree(test_folder_structure_name)
+    # Extract a pristine folder structure
+    with zipfile.ZipFile(test_folder_structure_zip, 'r') as zip_ref:
+        zip_ref.extractall("./tests")
+
+@pytest.fixture()
+def reset_folder_structure():
+    """Reset the folder structure to test on"""
+    _reset_folder_structure()
---
+++ tests/test_framework.py
@@ -0,0 +1,21 @@
+from conftest import _reset_folder_structure, test_folder_structure_name
+import os
+import shutil
+
+class TestFolderStructureReset:
+
+    def test_should_create_the_structure_if_not_there(self):
+        # ensure there's no folder structure
+        if os.path.isdir(test_folder_structure_name):
+            shutil.rmtree(test_folder_structure_name)
+        _reset_folder_structure()
+        assert os.path.isdir(test_folder_structure_name)
+
+    def test_should_reset_the_structure_if_already_there(self):
+        # ensure there's a folder structure to begin with
+        _reset_folder_structure()
+        testfile = "tests/Arma/testfile.txt"
+        with open(testfile, "w+") as f:
+            f.write("Hello there!")
+        _reset_folder_structure()
+        assert not os.path.isfile(testfile)
