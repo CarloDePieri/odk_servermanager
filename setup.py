381213307723afff5e0894473d8489e722297a88
Carlo De Pieri <depieri.carlo@gmail.com>
2020-02-15 12:40:07 +0100

    new name for the project to reflect the wider scope

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-import io
-import re
 from setuptools import setup
 
 setup(
-        name='arma_keysmanager',
+        name='odk_servermanager',
         version='0.1',
-        packages=['arma_keysmanager'],
+        packages=['odk_servermanager'],
         include_package_data=True,
         license='GPLv3',
         author='Carlo De Pieri',
-        description='Quickly set up keys in an arma server',
+        description='ODKclan tool to manage its Arma 3 server instances.',
         install_requires=[
             'beautifulsoup4==4.8.2'
             ],
         extras_require={
             'dev': [
-                'pytest>=4.0.0',
+                'pytest>=5.3.5',
+                'pytest-mock>=2.0.0',
                 'pytest-sugar>=0.9.2',
                 ]
             }
--- tests/test_km.py
+++ tests/test_km.py
@@ -1,4 +1,4 @@
-import arma_keysmanager.km as km
+import odk_servermanager.km as km
 from conftest import test_folder_structure_name, test_preset_file_name
 import os
 
@@ -46,10 +46,9 @@ class TestKeysSetUp:
 
     def test_should_first_clear_the_keys_folder_and_then_copy_the_keys_there(self, reset_folder_structure, mocker):
         """Keys set up should first clear the keys folder and then copy the keys there."""
-        mocker.patch("arma_keysmanager.km.clear_keys_folder", side_effect=km.clear_keys_folder)
-        mocker.patch("arma_keysmanager.km.copy_keys", side_effect=km.copy_keys)
-        mods_list = ["ODKAI",
-                     "ace"]
+        mocker.patch("odk_servermanager.km.clear_keys_folder", side_effect=km.clear_keys_folder)
+        mocker.patch("odk_servermanager.km.copy_keys", side_effect=km.copy_keys)
+        mods_list = ["ODKAI", "ace"]
         mods_folder = os.path.join(os.path.abspath(test_folder_structure_name), "!Workshop")
         keys_folder = os.path.join(os.path.abspath(test_folder_structure_name), "Keys")
         km.set_up_keys(mods_list, mods_folder, keys_folder)
