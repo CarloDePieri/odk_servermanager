d3432607cbd8b422b4d599f394c01c1501d4177f
Carlo De Pieri <depieri.carlo@gmail.com>
2020-02-16 18:38:27 +0100

    refactor + prepare core server folders

--- odk_servermanager/sm.py
+++ odk_servermanager/sm.py
@@ -1,4 +1,5 @@
-import os
+from os.path import join, isdir
+from os import listdir, mkdir
 
 
 class DuplicateServerName(Exception):
@@ -7,6 +8,7 @@ class DuplicateServerName(Exception):
 
 def symlink(source: str, link_name: str) -> None:
     """A symlink function that should work both on Windows and on Linux."""
+    import os
     os_symlink = getattr(os, "symlink", None)
     if callable(os_symlink):
         os_symlink(source, link_name)
@@ -15,15 +17,48 @@ def symlink(source: str, link_name: str) -> None:
         csl = ctypes.windll.kernel32.CreateSymbolicLinkW
         csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
         csl.restype = ctypes.c_ubyte
-        flags = 1 if os.path.isdir(source) else 0
+        flags = 1 if isdir(source) else 0
         if csl(link_name, source, flags) == 0:
             raise ctypes.WinError()
 
 
+def _make_server_name(server_name: str, server_root: str) -> str:
+    """Compose the server folder name."""
+    return join(server_root, "__server__" + server_name)
+
+
 def new_server_folder(server_name: str, server_root: str) -> None:
     """Create a new server folder."""
-    server_folder = os.path.join(server_root, "__server__" + server_name)
-    if not os.path.isdir(server_folder):
-        os.mkdir(server_folder)
+    server_folder = _make_server_name(server_name, server_root)
+    if not isdir(server_folder):
+        mkdir(server_folder)
     else:
         raise DuplicateServerName()
+
+
+def filter_symlinks(element: str) -> bool:
+    not_to_be_symlinked = [
+        "!Workshop",
+        "Keys"
+    ]
+    return not (element.startswith("__server__") or element in not_to_be_symlinked)
+
+
+def prepare_server_core(server_name: str, server_root: str) -> None:
+    # make all needed symlink
+    server_folder = _make_server_name(server_name, server_root)
+    folder_list = listdir(server_root)
+    to_be_linked = list(filter(lambda x: filter_symlinks(x), folder_list))
+    for el in to_be_linked:
+        src = join(server_root, el)
+        dest = join(server_folder, el)
+        symlink(src, dest)
+    # Create the needed folder
+    to_be_created = [
+        "Keys",
+        "!Mods_linked",
+        "!Mods_copied",
+    ]
+    for folder in to_be_created:
+        folder = join(server_folder, folder)
+        mkdir(folder)
--- tests/test_sm.py
+++ tests/test_sm.py
@@ -2,7 +2,7 @@ import pytest
 
 import odk_servermanager.sm as sm
 from conftest import test_folder_structure_path
-import os
+from os.path import islink, isfile, isdir, join
 
 
 class TestSymlinkFunction:
@@ -11,28 +11,50 @@ class TestSymlinkFunction:
     def test_should_create_a_symlink(self, reset_folder_structure):
         """Symlink function should create a symlink."""
         test_path = test_folder_structure_path()
-        src = os.path.join(test_path, "TestFolder1")
-        dest = os.path.join(test_path, "__server__TestServer0", "TestFolder1")
+        src = join(test_path, "TestFolder1")
+        dest = join(test_path, "__server__TestServer0", "TestFolder1")
         sm.symlink(src, dest)
-        assert os.path.islink(dest)
-        assert os.path.isfile(os.path.join(dest, "testFile1.txt"))
+        assert islink(dest)
+        assert isfile(join(dest, "testFile1.txt"))
 
 
 class TestCreateInstance:
     """Test: create instance..."""
+    test_path = test_folder_structure_path()
 
     def test_should_create_a_new_folder_with_a_given_name(self, reset_folder_structure):
         """Create instance should create a new folder with a given name."""
-        test_path = test_folder_structure_path()
         server_name = "TestServer1"
-        sm.new_server_folder(server_name, test_path)
-        assert os.path.isdir(os.path.join(test_path, "__server__" + server_name))
+        sm.new_server_folder(server_name, self.test_path)
+        assert isdir(join(self.test_path, "__server__" + server_name))
 
     def test_should_raise_an_error_with_an_already_existing_server_instance(self, reset_folder_structure):
         """Create instance should raise an error with an already existing server instance."""
-        test_path = test_folder_structure_path()
         server_name = "TestServer0"
         from odk_servermanager.sm import DuplicateServerName
         with pytest.raises(DuplicateServerName) as err:
-            sm.new_server_folder(server_name, test_path)
+            sm.new_server_folder(server_name, self.test_path)
 
+    @pytest.mark.runthis
+    def test_should_symlink_and_create_all_needed_stuff_from_the_main_folder(self, reset_folder_structure):
+        """Create instance should symlink and create all needed stuff from the main folder."""
+        server_name = "TestServer0"
+        server_folder = join(self.test_path, "__server__" + server_name)
+        sm.prepare_server_core(server_name, self.test_path)
+        # Check that it didn't copy over the wrong stuff
+        assert not isdir(join(server_folder, "!Workshop"))
+        assert not isdir(join(server_folder, "__server__TestServer0"))
+        # Check all symlink
+        assert islink(join(server_folder, "TestFolder1"))
+        assert islink(join(server_folder, "TestFolder2"))
+        assert islink(join(server_folder, "testFile0.txt"))
+        assert islink(join(server_folder, "testFile3.txt"))
+        assert isfile(join(server_folder, "TestFolder1", "testFile1.txt"))
+        assert isfile(join(server_folder, "TestFolder2", "testFile2.txt"))
+        # Check folders
+        keys_folder = join(server_folder, "Keys")
+        mods_linked_folder = join(server_folder, "!Mods_linked")
+        mods_copied_folder = join(server_folder, "!Mods_copied")
+        assert isdir(keys_folder) and not islink(keys_folder)
+        assert isdir(mods_copied_folder) and not islink(mods_copied_folder)
+        assert isdir(mods_linked_folder) and not islink(mods_linked_folder)
