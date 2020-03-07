42c5a7c7daf6d68c3976f31171281d0c3d12511e
Carlo De Pieri <depieri.carlo@gmail.com>
2020-03-07 18:05:23 +0100

    continued with the readme + small fixes

--- ODKSM.bat
+++ ODKSM.bat
@@ -2,20 +2,29 @@
 ::                                                                                           |
 ::                                 == ODK SERVER MANAGER ==                                  |
 ::                                                                                           |
-::     Leave this here. Symlink it to your preferred directory and launch it from there!     |
+::      Use this bat to launch the tool. Remember to pass in a config.ini file to it!        |
 ::--------------------------------------------------------------------------------------------
 @echo off
 IF %1.==. GOTO No1
 
+:: If you move the ODKSM.bat remember to fill in ODKSM_ROOT_PATH: it should be the absolute path to the folder that
+:: contains the run.py file
+set ODKSM_ROOT_PATH="."
+
+:: Recover the config file absolute path
 set CONFIG=%~dpfn1
+
+cd "%ODKSM_ROOT_PATH%"
 pipenv run python run.py "%CONFIG%"
 
 GOTO End1
 
 :No1
   ECHO.
-  ECHO You tried to call this program directly :(
-  ECHO You need to pass in a config file as first argument, either by cmd, batch file or dragging.
+  ECHO ======[ WELCOME TO ODKSM! ]======
+  ECHO.
+  ECHO [ERR] You tried to call this program directly :(
+  ECHO You need to pass in a config file as first argument, either by cmd, batch file or drag^&drop.
   ECHO.
   ECHO Bye!
   ECHO.
