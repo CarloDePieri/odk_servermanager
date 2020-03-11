cfbf43a680946401356f7f2dec52943b9d018a2c
Carlo De Pieri <depieri.carlo@gmail.com>
2020-03-11 12:47:00 +0100

    moved the odkai_local modfix to link hooks

--- odk_servermanager/modfix/odkai_local.py
+++ odk_servermanager/modfix/odkai_local.py
@@ -10,19 +10,19 @@ class ModFixODKAILocal(ModFix):
 
     name: str = "ODKAI"
 
-    def hook_init_copy_replace(self, server_instance: ServerInstance) -> None:
+    def hook_init_link_replace(self, server_instance: ServerInstance) -> None:
         """Used to symlink a local version of ODKAI in the instance.
 
         This hook will look for following fields in mod_fix_settings:
         :odkai_local_path: the full path of the folder containing the mod
         """
-        mod_folder = join(server_instance.get_server_instance_path(), server_instance.S.copied_mod_folder_name,
+        mod_folder = join(server_instance.get_server_instance_path(), server_instance.S.linked_mod_folder_name,
                           "@" + self.name)
         mod_fix_settings = server_instance.S.fix_settings.mod_fix_settings
         local_folder = abspath(mod_fix_settings["odkai_local_path"])
         symlink(local_folder, mod_folder)
 
-    def hook_update_copy_replace(self, server_instance: ServerInstance) -> None:
+    def hook_update_linked_replace(self, server_instance: ServerInstance) -> None:
         """This empty hook will prevent the update of an already there ODKAI.
         This is because the mod is actually a symlink and does not need to be updated."""
         pass
--- tests/modfixes/test_modfix_odkai_local.py
+++ tests/modfixes/test_modfix_odkai_local.py
@@ -24,15 +24,15 @@ class TestAModFixOdkaiLocal(ODKSMTest):
         fix_settings = ModFixSettings(enabled_fixes=["odkai_local"],
                                       mod_fix_settings={"odkai_local_path": local_folder})
         settings = ServerInstanceSettings("test", c_sb_stub, c_sc_stub, user_mods_list=["ODKAI"],
-                                          mods_to_be_copied=["ODKAI"], arma_folder=self.test_path,
+                                          arma_folder=self.test_path,
                                           server_instance_root=self.test_path,
                                           fix_settings=fix_settings)
         request.cls.instance = ServerInstance(settings)
         self.instance._new_server_folder()
         self.instance._prepare_server_core()
-        self.instance._apply_hooks_and_do_op("init", "copy", "ODKAI")
+        self.instance._start_op_on_mods("init", ["ODKAI"])
         request.cls.mod_folder = join(self.instance.get_server_instance_path(),
-                                      self.instance.S.copied_mod_folder_name, "@ODKAI")
+                                      self.instance.S.linked_mod_folder_name, "@ODKAI")
 
     def test_should_symlink_the_local_copy(self):
         """A mod fix odkailocal should symlink the local copy."""
@@ -40,5 +40,5 @@ class TestAModFixOdkaiLocal(ODKSMTest):
 
     def test_should_do_nothing_on_update(self):
         """A mod fix odkai local should do nothing on update."""
-        self.instance._apply_hooks_and_do_op("update", "copy", "ODKAI")
+        self.instance._start_op_on_mods("update", ["ODKAI"])
         assert islink(self.mod_folder)
